3dca9d286205675adae0f05153e9f9d2b069ebf4
Chloe Kelly <chloetay3@gmail.com>
2021-02-03 17:44:41 -0500

    Add random background colors for highlightCell()

--- sketch.js
+++ sketch.js
@@ -13,6 +13,32 @@ gridBtn.forEach((button) => { //Is there a singular form of "forEach"-- seems wr
   return parseInt(input);
 }*/
 
+//Function to refresh the page for the next grid.  NEED THIS???
+/*function refreshPage() {
+  window.location.reload();
+}*/ 
+
+//Function to clear the previous grid. WORKS, BUT DOESN'T REFRESH THE PAGE. NEW GRID WON'T APPEAR AFTER THE PROMPT.
+function clearGrid() {
+  let previousGrid = document.querySelector("div");
+  previousGrid.remove();
+}
+
+//Function to clear previous grid EXP 2.  NO.
+/*function clearGrid() {
+  let previousGrid = document.getElementsByName("cell");
+  previousGrid.remove();
+}*/
+
+//Function to clear the HIGHLIGHT. EXP 3. NO.
+/*function clear() {
+  let previousFill = document.getElementsByClassName("highlight");
+  previousFill.remove();
+}*/
+
+//EXP 4 ???
+
+
 //Function for user to input the number of squares for grid sides with limit of 100.  WORKS!  How can I make it keep asking for new input if the user keeps putting in the wrong number?
 function userInput() {
   let input = prompt("Choose a grid size");
@@ -29,10 +55,17 @@ function calcCellSize() {
   return 100 / gridSize;
 }
 
-//Function to change color of cell on mouseover
-function highlightCell(event) {
+//Function to change color of cell on mouseover ORIGINAL
+/*function highlightCell(event) {
   let cell = event.target;
   cell.classList.add("highlight");
+}*/
+
+//Function to fill cell with random background color on mouseover EXP.  WORKS!!!
+function highlightCell(event) {
+  let cell = event.target;
+  let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  cell.style.backgroundColor = randomColor;
 }
 
 //Function to create a square grid of boxes
@@ -47,16 +80,11 @@ function makeGrid(size) {
   }
 }
 
-//Function to refresh the page for the next grid
-function refreshPage() {
-  window.location.reload();
-}
- 
 //Function that combines smaller functions into big fx called sketch(), which is called with button click
 function sketch() {
   
   let gridSize = userInput();
-  
+
   function calcCellSize() {
     return 100 / gridSize;
   }
@@ -68,7 +96,8 @@ function sketch() {
 
   function highlightCell(event) {
     let cell = event.target;
-    cell.classList.add("highlight");
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    cell.style.backgroundColor = randomColor;
   }
 
   function makeGrid(size) {
